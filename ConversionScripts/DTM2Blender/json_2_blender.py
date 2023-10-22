1945111a122f8625694c8d1cb0a40dad8cda72c5
jupiterbjy <jupiterbjy@gmail.com>
2023-10-23 05:03:49 +0900

    Overhaul on DTM2Blender's blender-side script to allow proper GLTF animation exporting

--- ConversionScripts/DTM2Blender/json_2_blender.py
+++ ConversionScripts/DTM2Blender/json_2_blender.py
@@ -2,15 +2,18 @@
 Blender script for rebuilding json-ed *.dtm files back into shape
 """
 
+import os
 import math
 import json
 import pathlib
+import tempfile
 
 import bpy
 import bmesh
+import addon_utils
 
 
-# CONFIG ---------
+# CONFIG/ENV ---------
 
 # change path to your dtm2json output path
 PATH = r"E:\github\OpenAT\ConversionScripts\DTM2Blender\dtm2json_output"
@@ -18,6 +21,8 @@ PATH = pathlib.Path(PATH)
 
 PATH_LIST = PATH / "_files.txt"
 ENCODING = "utf8"
+
+TEMP_FD, TEMP_FILE = tempfile.mkstemp(suffix=".mdd")
 
 
 # shadow print() for output redirection to UI ---------
@@ -36,12 +41,77 @@ def print(*args, sep=""):
         bpy.ops.console.scrollback_append(override, text=sep.join(map(str, args)), type="OUTPUT")
 
 
-# main functions ---------
+# make sure NewTek MDD format is enabled ---------
+
+if not addon_utils.check("io_shape_mdd")[1]:
+    print("Enabling MDD format addon")
+    addon_utils.enable("io_shape_mdd")
+else:
+    print("MDD format addon already loaded")
+
+
+# util functions ---------
 
 def yield_files_gen():
     """path list yielding iterator"""
     for raw_path in PATH_LIST.read_text(ENCODING).splitlines():
         yield pathlib.Path(raw_path)
+
+
+def cleanup_temp():
+    """Remove temp file"""
+
+    file = pathlib.Path(TEMP_FILE)
+
+    try:
+        file.unlink(missing_ok=True)
+
+    except PermissionError:
+        # need few more steps
+        # ref: https://stackoverflow.com/a/50520072/10909029
+        os.close(TEMP_FD)
+        file.unlink()
+
+
+# main functions ---------
+
+def vertex_anim_to_shapekey(obj, frame_count):
+    """Convert vertex animation into shapekeys"""
+
+    # select & set active
+    obj.select_set(True)
+    bpy.context.view_layer.objects.active = obj
+
+    # export
+    print("Exporting mdd")
+    bpy.ops.export_shape.mdd(filepath=TEMP_FILE, fps=11, frame_end=frame_count)
+
+    # remove all keyframes
+    for vertex in obj.data.vertices:
+        for frame_no in range(1, frame_count + 1):
+            vertex.keyframe_delete("co", frame=frame_no)
+
+    # import - we want this start from 0 so gltf export can use frame 0 as resting position.
+    print("Importing mdd")
+    bpy.ops.import_shape.mdd(filepath=TEMP_FILE)
+
+    # delete unnecessary basis key - that's frame 0 for us
+    obj.active_shape_key_index = 0
+    bpy.ops.object.shape_key_remove()
+
+    # took entire night to figure this out, not on Google at least...
+    # we need to remove -1 keyframe from frame_0000, that's duplicate.
+    # also, remove last frame's last key, as this key resets animation back to frame 0.
+    delete_target = {
+        "frame_0000": -1,
+        f"frame_{frame_count - 1:04}": frame_count
+    }
+
+    for key, frame in delete_target.items():
+        obj.data.shape_keys.key_blocks[key].keyframe_delete("value", frame=frame)
+
+    # unselect
+    obj.select_set(False)
 
 
 def reconstruct_uv(data: dict, mesh):
@@ -70,25 +140,25 @@ def reconstruct_uv(data: dict, mesh):
     bm.to_mesh(mesh)
 
 
-def insert_keyframes(keyframes, obj):
+def insert_keyframes(non_rest_keyframes, obj):
     """Add animation to obj"""
 
-    print(f"Inserting {len(keyframes)} more keyframes")
-
-    vertices = obj.data.vertices
+    print(f"Inserting {len(non_rest_keyframes)} more keyframes")
 
     # insert current state as first shape key
-    for vertex in vertices:
+    for vertex in obj.data.vertices:
         # co stand for coordinates from what I see in manual
         vertex.keyframe_insert("co", frame=1)
 
     # fill in other frames
-    for frame_no, frame in enumerate(keyframes, 2):
-        print(f"Inserting frame {frame_no}")
+    for frame_no, frame in enumerate(non_rest_keyframes, 2):
 
-        for vertex, frame_pos in zip(vertices, frame):
+        for vertex, frame_pos in zip(obj.data.vertices, frame):
             vertex.co = frame_pos
             vertex.keyframe_insert("co", frame=frame_no)
+
+    # convert so it can be exported in gltf
+    vertex_anim_to_shapekey(obj, len(non_rest_keyframes) + 1)
 
 
 def reconstruct(file_path: pathlib.Path):
@@ -113,23 +183,27 @@ def reconstruct(file_path: pathlib.Path):
     reconstruct_uv(data, mesh)
     obj = bpy.data.objects.new(file_path.stem, mesh)
 
+    # add object to main collections so we can select individually
+    scene = bpy.context.scene
+    scene.collection.objects.link(obj)
+
     # insert keyframe if there's more than one frame
     if other_keyframes:
         insert_keyframes(other_keyframes, obj)
 
-    # scale
+    # set correct scale
     scale = data["scale"]
     obj.scale = scale, scale, scale
 
     # adjust rotation since blender use difference axis orders
     obj.rotation_euler = (math.radians(90), 0, 0)
 
-    # add object to main collections
-    scene = bpy.context.scene
-    scene.collection.objects.link(obj)
 
+# Driver --------
 
-# generate objects
-for _path in yield_files_gen():
-    print(f"Reconstructing {_path}")
-    reconstruct(_path)
+try:
+    for _path in yield_files_gen():
+        print(f"Reconstructing {_path}")
+        reconstruct(_path)
+finally:
+    cleanup_temp()
